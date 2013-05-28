b683c4f70ab997dd89e907f6efa748392828f5c6
David Heyman <dave@axismaps.com>
2013-05-28 21:38:29 +0100

    Larger zoom levels and TMS tiles

--- js/map.js
+++ js/map.js
@@ -9,8 +9,8 @@ function setupMap(){
 	map = L.map('map').fitBounds( L.latLngBounds( L.latLng(17.644, -78.409), L.latLng(18.589, -76.190) ) );
 	//.setView([18.188, -77.363], 10);
 	map.setZoom( Math.max( map.getZoom(), 9 ) );
-	placesMap = L.tileLayer('tiles/placenames/{z}/{x}/{y}.png', {maxZoom: 12, minZoom: 7} ).addTo(map);
-	terrainMap = L.tileLayer('tiles/terrain/{z}/{x}/{y}.png', {maxZoom: 12, minZoom: 7} );
+	placesMap = L.tileLayer('tiles/placenames/{z}/{x}/{y}.png', {maxZoom: 13, minZoom: 7, tms: true} ).addTo(map);
+	terrainMap = L.tileLayer('tiles/terrain/{z}/{x}/{y}.png', {maxZoom: 13, minZoom: 7, tms: true} );
 	mapLayers = L.layerGroup().addTo(map);
 }
 
